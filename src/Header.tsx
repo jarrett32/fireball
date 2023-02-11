be8f7fda8fffbaf8d1fea49310417bc07b848e40
jarrett32 <jarrettthompson32@gmail.com>
2023-02-10 20:21:22 -0800

    update

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,15 +3,19 @@ import React from "react";
 const Header = () => {
   return (
     <div>
+      <div className="background absolute z-0 h-full w-full opacity-75"></div>
       <div className="p-32"></div>
-      <h1 className="text-8xl font-extrabold text-white">Fireball</h1>
-      <p className="my-2 w-96 text-white">
-        Fireball Fireball Fireball Fireball Fireball Fireball Fireball Fireball
-        Fireball Fireball Fireball Fireball Fireball Fireball Fireball
-      </p>
-      <button className="my-6 cursor-pointer bg-green-500 px-4 py-2 text-white">
-        Click Here Guy
-      </button>
+      <div className="relative z-10">
+        <h1 className="text-8xl font-extrabold text-orange-500">Fireball</h1>
+        <p className="my-2 w-96 text-orange-500">
+          Fireball Fireball Fireball Fireball Fireball Fireball Fireball
+          Fireball Fireball Fireball Fireball Fireball Fireball Fireball
+          Fireball
+        </p>
+        <button className="my-6 cursor-pointer bg-orange-500 px-4 py-2 text-white">
+          Click Here
+        </button>
+      </div>
     </div>
   );
 };
