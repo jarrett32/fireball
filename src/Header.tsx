52aad7ce8765c3a45d8342ecd9ddb05ce7be874a
jarrett32 <jarrettthompson32@gmail.com>
2023-02-10 20:08:04 -0800

    update

---
+++ src/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="p-32"></div>
+      <h1 className="text-8xl font-extrabold text-white">Fireball</h1>
+      <p className="my-2 w-96 text-white">
+        Fireball Fireball Fireball Fireball Fireball Fireball Fireball Fireball
+        Fireball Fireball Fireball Fireball Fireball Fireball Fireball
+      </p>
+      <button className="my-6 cursor-pointer bg-green-500 px-4 py-2 text-white">
+        Click Here Guy
+      </button>
+    </div>
+  );
+};
+
+export default Header;
